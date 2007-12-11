27e5e39ead4ae7d4674a6c2bb6f35e942e18fe94
Roland Riegel <roland@roland-riegel.de>
2007-12-11 13:17:26 +0000

    implement hpux device detection read packet error and drop counts as well some cleanups in device reading code

--- src/devreader-hpux.cpp
+++ src/devreader-hpux.cpp
@@ -78,6 +78,15 @@
 
 using namespace std;
 
+#define AREA_SIZE 40000
+#define LONG_AREA_SIZE  (AREA_SIZE / sizeof(u_long))
+
+static u_long ctrl_area[LONG_AREA_SIZE];        /* for control messages */
+static u_long data_area[LONG_AREA_SIZE];        /* for data messages */
+static u_long ppa_area[LONG_AREA_SIZE];         /* for saving ppa area */
+static struct strbuf ctrl_buf = {AREA_SIZE, 0, (char*) ctrl_area};
+static struct strbuf data_buf = {AREA_SIZE, 0, (char*) data_area};
+
 DevReaderHpux::DevReaderHpux(const string& deviceName)
     : DevReader(deviceName)
 {
@@ -89,53 +98,99 @@ DevReaderHpux::~DevReaderHpux()
 
 list<string> DevReaderHpux::findAllDevices()
 {
-    // TODO: implement device scanning for HPUX
-    return list<string>(1, "lan0");
+    list<string> devicesFound;
+    
+    dl_hp_ppa_info_t* ppa_info_temp;
+    dl_hp_ppa_req_t* ppa_req;
+    dl_hp_ppa_ack_t* ppa_ack;
+    dl_attach_req_t* attach_req;
+    
+    int fd = -1;
+    if((fd = open("/dev/dlpi", O_RDWR)) < 0)
+    {
+        perror("open /dev/dlpi");
+        return devicesFound;
+    }
+    
+    do
+    {
+        ppa_req = (dl_hp_ppa_req_t*) ctrl_area;
+        ppa_ack = (dl_hp_ppa_ack_t*) ctrl_area;
+        
+        ppa_req->dl_primitive = DL_HP_PPA_REQ;
+        
+        ctrl_buf.len = sizeof(dl_hp_ppa_req_t);
+        if(putmsg(fd, &ctrl_buf, 0, 0) < 0)
+        {
+            perror("putmsg DL_HP_PPA_REQ");
+            break;
+        }
+        
+        int flags = 0;
+        ctrl_area[0] = 0;
+        
+        if(getmsg(fd, &ctrl_buf, &data_buf, &flags) < 0)
+        {
+            perror("getmsg DL_HP_PPA_REQ");
+            break;
+        }
+        
+        if(ppa_ack->dl_length == 0)
+        {
+            fprintf(stderr, "error: no PPAs available\n");
+            break;
+        }
+        
+        // save all the PPA information
+        memcpy((u_char*) ppa_area, (u_char*) ctrl_area + ppa_ack->dl_offset, ppa_ack->dl_length);
+        int ppa_count = ppa_ack->dl_count;
+        
+        int count;
+        for(count = 0, ppa_info_temp = (dl_hp_ppa_info_t*) ppa_area; count < ppa_count; count++, ppa_info_temp++)
+            devicesFound.push_back(string("lan") + toString(ppa_info_temp->dl_ppa));
+
+    } while(0);
+
+    close(fd);
+
+    return devicesFound;
 }
 
 void DevReaderHpux::readFromDevice(DataFrame& dataFrame)
 {
-    #define AREA_SIZE 40000
-    #define LONG_AREA_SIZE  (AREA_SIZE / sizeof(u_long))
-    #define TRUE 1
-    
-    static u_long ctrl_area[LONG_AREA_SIZE];        /* for control messages */
-    static u_long data_area[LONG_AREA_SIZE];        /* for data messages */
-    static u_long ppa_area[LONG_AREA_SIZE];         /* for saving ppa area */
-    static struct strbuf ctrl_buf = {AREA_SIZE, 0, (char*) ctrl_area};
-    static struct strbuf data_buf = {AREA_SIZE, 0, (char*) data_area};
-    
-    dl_get_statistics_req_t *get_statistics_req = (dl_get_statistics_req_t *) ctrl_area;
-    dl_get_statistics_ack_t *get_statistics_ack = (dl_get_statistics_ack_t *) ctrl_area;
-    
-    dl_hp_ppa_info_t ppa_info, *ppa_info_temp;
-    dl_hp_ppa_req_t *ppa_req;
-    dl_hp_ppa_ack_t *ppa_ack;
-    dl_attach_req_t *attach_req;
-    mib_ifEntry     *mib_ptr;
+    dl_get_statistics_req_t* get_statistics_req = (dl_get_statistics_req_t*) ctrl_area;
+    dl_get_statistics_ack_t* get_statistics_ack = (dl_get_statistics_ack_t*) ctrl_area;
+
+    dl_hp_ppa_info_t ppa_info;
+    dl_hp_ppa_info_t* ppa_info_temp;
+    dl_hp_ppa_req_t* ppa_req;
+    dl_hp_ppa_ack_t* ppa_ack;
+    dl_attach_req_t* attach_req;
+    mib_ifEntry* mib_ptr;
     
     unsigned int ppa = 0;
-    int fd = 0, flags = 0, ppa_count = 0, count = 0, found = 0;
+    int flags = 0, ppa_count = 0, count = 0;
+    
+    // make sure interface name begins with lan
+    if(m_deviceName.length() < 3 || m_deviceName.substr(0, 3) != "lan")
+        return;
+    
+    // get the PPA from the interface name
+    ppa = fromString<int>(m_deviceName.substr(3));
+    if(ppa == 0 && m_deviceName != "lan0")
+        return;
+    
+    int fd = -1;
+    if((fd = open("/dev/dlpi", O_RDWR)) < 0)
+    {
+        perror("open /dev/dlpi");
+        return;
+    }
     
     do
     {
-        //Make sure interface name begins with lan
-        if(m_deviceName.length() < 3 || m_deviceName.substr(0, 3) != "lan")
-            break;
-        
-        //Get the PPA from the interface name
-        ppa = fromString<int>(m_deviceName.substr(3));
-        if(ppa == 0 && m_deviceName != "lan0")
-            break;
-        
-        if((fd = open("/dev/dlpi", O_RDWR)) < 0)
-        {
-            perror("Open /dev/dlpi");
-            break;
-        }
-        
-        ppa_req = (dl_hp_ppa_req_t *)ctrl_area;
-        ppa_ack = (dl_hp_ppa_ack_t *)ctrl_area;
+        ppa_req = (dl_hp_ppa_req_t*) ctrl_area;
+        ppa_ack = (dl_hp_ppa_ack_t*) ctrl_area;
         
         ppa_req->dl_primitive = DL_HP_PPA_REQ;
         
@@ -161,25 +216,26 @@ void DevReaderHpux::readFromDevice(DataFrame& dataFrame)
             break;
         }
         
-        //Save all the PPA information.
-        memcpy((u_char *)ppa_area, (u_char *)ctrl_area+ppa_ack->dl_offset, ppa_ack->dl_length);
+        // save all the PPA information
+        memcpy((u_char*) ppa_area, (u_char*) ctrl_area + ppa_ack->dl_offset, ppa_ack->dl_length);
         ppa_count = ppa_ack->dl_count;
         
-        for(count = found = 0, ppa_info_temp = (dl_hp_ppa_info_t *)ppa_area; count<ppa_count; count++, ppa_info_temp++)
+        bool found = false;
+        for(count = 0, ppa_info_temp = (dl_hp_ppa_info_t*) ppa_area; count < ppa_count; count++, ppa_info_temp++)
         {
             if(ppa_info_temp->dl_ppa == ppa)
             {
-                found = TRUE;
+                found = true;
                 break;
             }
         }
         if(!found)
         {
-            fprintf(stderr, "Error: PPA %d not found\n", ppa);
+            fprintf(stderr, "error: PPA %d not found\n", ppa);
             break;
         }
         
-        attach_req = (dl_attach_req_t *) ctrl_area;
+        attach_req = (dl_attach_req_t*) ctrl_area;
         attach_req->dl_primitive = DL_ATTACH_REQ;
         attach_req->dl_ppa = ppa;
         ctrl_buf.len = sizeof(dl_attach_req_t);
@@ -213,17 +269,26 @@ void DevReaderHpux::readFromDevice(DataFrame& dataFrame)
             break;
         }
         if(get_statistics_ack->dl_primitive != DL_GET_STATISTICS_ACK)
-            fprintf(stderr, "Error: Wrong primitive\n");
+            fprintf(stderr, "error: wrong primitive\n");
 
-        mib_ptr = (mib_ifEntry *)((u_char *) ctrl_area + get_statistics_ack->dl_stat_offset);
-
-        close(fd);
+        mib_ptr = (mib_ifEntry*) ((u_char*) ctrl_area + get_statistics_ack->dl_stat_offset);
 
         dataFrame.setTotalDataIn(mib_ptr->ifInOctets);
         dataFrame.setTotalDataOut(mib_ptr->ifOutOctets);
 
+        dataFrame.setTotalPacketsIn(mib_ptr->ifInUcastPkts + mib_ptr->ifInNUcastPkts);
+        dataFrame.setTotalPacketsOut(mib_ptr->ifOutUcastPkts + mib_ptr->ifOutNUcastPkts);
+
+        dataFrame.setTotalErrorsIn(mib_ptr->ifInErrors);
+        dataFrame.setTotalErrorsOut(mib_ptr->ifOutErrors);
+
+        dataFrame.setTotalDropsIn(mib_ptr->ifInDiscards);
+        dataFrame.setTotalDropsOut(mib_ptr->ifOutDiscards);
+
         dataFrame.setValid(true);
 
     } while(0);
+
+    close(fd);
 }
 
