ed4a99ee3bf6e3599eaead21e2bb882705761917
Roland Riegel <roland@roland-riegel.de>
2007-12-14 11:45:10 +0000

    rewrite the linux proc devreader using c++ style

--- src/devreader-linux-proc.cpp
+++ src/devreader-linux-proc.cpp
@@ -7,22 +7,16 @@
  *                                                                         *
  ***************************************************************************/
 
-/***************************************************************************
- * Linux specific network code taken out of knetload:                      *
- * Copyright by Markus Gustavsson <mighty@fragzone.se>                     *
- * adapted for nload by Roland Riegel <feedback@roland-riegel.de>          *
- ***************************************************************************/
-
 #include "devreader-linux-proc.h"
+#include "stringutils.h"
 
-#include <stdio.h>
-#include <string.h>
+#include <fstream>
+#include <list>
+#include <string>
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-
-#include <string>
-#include <list>
 
 using namespace std;
 
@@ -48,119 +42,115 @@ list<string> DevReaderLinuxProc::findAllDevices()
 {
     list<string> interfaceNames;
     
-    FILE* fd = 0;
-    char buf[512] = "";
-    char dev[128] = "";
-    char* tmp = 0;
-    char* tmp2 = 0;
-
-    if(!(fd = fopen("/proc/net/dev", "r")))
+    ifstream fin("/proc/net/dev");
+    if(!fin.is_open())
         return interfaceNames;
     
     // skip the two header lines
-    fgets(buf, sizeof(buf), fd);
-    fgets(buf, sizeof(buf), fd);
-    
-    while(fgets(buf, sizeof(buf), fd))
+    string line;
+    getline(fin, line);
+    getline(fin, line);
+
+    if(!fin.good())
+        return interfaceNames;
+
+    // read all remaining lines and extract the device name
+    while(fin.good())
     {
-        memset(dev, 0, sizeof(dev));
-        
-        tmp = buf;
-        tmp2 = dev;
-       
-        // skip whitespace
-        while(*tmp == ' ')
-            tmp++;
-        // extract device name
-        while((*tmp2++ = *tmp++) != ':');
-        *--tmp2 = '\0';
-    
-        interfaceNames.push_back(string(dev));
+        getline(fin, line);
+        if(line.empty())
+            continue;
+
+        string::size_type posEnd = line.find(':');
+        if(posEnd == string::npos)
+            continue;
+
+        interfaceNames.push_back(trim(line.substr(0, posEnd)));
     }
     
-    fclose(fd);
-
     return interfaceNames;
 }
 
 void DevReaderLinuxProc::readFromDevice(DataFrame& dataFrame)
 {
-    FILE* fd = 0;
-    char buf[512] = "";
-    char dev[128] = "";
-    char* tmp = 0;
-    char* tmp2 = 0;
-
     if(m_deviceName.empty())
         return;
     
-    do
+    ifstream fin("/proc/net/dev");
+    if(!fin.is_open())
+        return;
+    
+    // skip the two header lines
+    string line;
+    getline(fin, line);
+    getline(fin, line);
+
+    if(!fin.good())
+        return;
+
+    // search for device
+    while(fin.good())
     {
-        if(!(fd = fopen("/proc/net/dev", "r")))
+        getline(fin, line);
+        if(line.empty())
+            continue;
+
+        string::size_type posEnd = line.find(':');
+        if(posEnd == string::npos)
+            continue;
+
+        string interfaceName = trim(line.substr(0, posEnd));
+        if(interfaceName.empty())
+            continue;
+
+        // check if it is the device we want
+        if(m_deviceName != interfaceName)
+            continue;
+
+        // read device data
+        unsigned int bytesIn = 0;
+        unsigned int packetsIn = 0;
+        unsigned int errorsIn = 0;
+        unsigned int dropsIn = 0;
+        unsigned int bytesOut = 0;
+        unsigned int packetsOut = 0;
+        unsigned int errorsOut = 0;
+        unsigned int dropsOut = 0;
+        unsigned int dummy = 0;
+
+        istringstream sin(trim(line.substr(posEnd + 1)));
+
+        sin >> bytesIn
+            >> packetsIn
+            >> errorsIn
+            >> dropsIn
+            >> dummy
+            >> dummy
+            >> dummy
+            >> dummy
+            >> bytesOut
+            >> packetsOut
+            >> errorsOut
+            >> dropsOut;
+
+        if(sin.fail())
             break;
         
-        // skip the two header lines
-        fgets(buf, sizeof(buf), fd);
-        fgets(buf, sizeof(buf), fd);
+        dataFrame.setTotalDataIn(bytesIn);
+        dataFrame.setTotalDataOut(bytesOut);
+
+        dataFrame.setTotalPacketsIn(packetsIn);
+        dataFrame.setTotalPacketsOut(packetsOut);
+
+        dataFrame.setTotalErrorsIn(errorsIn);
+        dataFrame.setTotalErrorsOut(errorsOut);
         
-        while(fgets(buf, sizeof(buf), fd))
-        {
-            memset(dev, 0, sizeof(dev));
-            
-            tmp = buf;
-            tmp2 = dev;
-           
-            // skip whitespace
-            while(*tmp == ' ')
-                tmp++;
-            // extract device name
-            while((*tmp2++ = *tmp++) != ':');
-            *--tmp2 = '\0';
-            
-            // check if it is the device we want
-            if(strcmp(m_deviceName.c_str(), dev) == 0)
-            {
-                unsigned long bytesIn = 0;
-                unsigned long packetsIn = 0;
-                unsigned long errorsIn = 0;
-                unsigned long dropsIn = 0;
-                unsigned long bytesOut = 0;
-                unsigned long packetsOut = 0;
-                unsigned long errorsOut = 0;
-                unsigned long dropsOut = 0;
-                
-                // read device data
-                sscanf(tmp, "%lu %lu %lu %lu %*u %*u %*u %*u %lu %lu %lu %lu",
-                   &bytesIn,
-                   &packetsIn,
-                   &errorsIn,
-                   &dropsIn,
-                   &bytesOut,
-                   &packetsOut,
-                   &errorsOut,
-                   &dropsOut
-                );
-
-                dataFrame.setTotalDataIn(bytesIn);
-                dataFrame.setTotalDataOut(bytesOut);
-
-                dataFrame.setTotalPacketsIn(packetsIn);
-                dataFrame.setTotalPacketsOut(packetsOut);
-
-                dataFrame.setTotalErrorsIn(errorsIn);
-                dataFrame.setTotalErrorsOut(errorsOut);
-                
-                dataFrame.setTotalDropsIn(dropsIn);
-                dataFrame.setTotalDropsOut(dropsOut);
-                
-                dataFrame.setValid(true);
-                
-                break;
-            }
-        }
+        dataFrame.setTotalDropsIn(dropsIn);
+        dataFrame.setTotalDropsOut(dropsOut);
         
-        fclose(fd);
-
-    } while(0);
+        dataFrame.setValid(true);
+        
+        break;
+    }
 }
 
