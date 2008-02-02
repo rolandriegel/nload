83f6b80670aaf855ad6f2df744c6b0ff02d8c3e3
Roland Riegel <roland@roland-riegel.de>
2008-02-02 16:14:34 +0000

    update copyright

--- src/devreader.cpp
+++ src/devreader.cpp
@@ -1,3 +1,19 @@
+/***************************************************************************
+                                devreader.cpp
+                             -------------------
+    begin                : Fri Nov 16 2007
+    copyright            : (C) 2007 - 2008 by Roland Riegel
+    email                : feedback@roland-riegel.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #include "devreader.h"
 
--- src/settingfilter.cpp
+++ src/settingfilter.cpp
@@ -1,3 +1,19 @@
+/***************************************************************************
+                              settingfilter.cpp
+                             -------------------
+    begin                : Wed Nov 28 2007
+    copyright            : (C) 2007 - 2008 by Roland Riegel
+    email                : feedback@roland-riegel.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #include "settingfilter.h"
 #include "stringutils.h"
